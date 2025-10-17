87946c2b53ffa2b24a38ffc130a6ae1da9f46ce6
yorleny del carmen araujo vera <yorlenyvera.inc@gmail.com>
2025-10-16 22:25:11 -0500

    updated

--- src/app/components/scene/Scene.tsx
+++ src/app/components/scene/Scene.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Suspense, useEffect, useState, useRef } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
